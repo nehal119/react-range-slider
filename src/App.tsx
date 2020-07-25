3fd5ef5457a66ae1a9e9562c77c0a28610953e0e
Nehal Ahmad <nehal.ahmad@arnowa.com>
2020-07-25 20:07:51 +0530

    minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Nouislider from "nouislider-react";
 import "nouislider/distribute/nouislider.css";
 import './App.css';
+
 export default function App() {
   const firstValue = 50;
   const sliderStart = 0;
@@ -49,7 +50,6 @@ export default function App() {
     alert("Sending value")
   }
   console.log(percent);
-  // console.log(bcolor);
 
   return (
     <div className="App" style={{marginTop: "100px", position: 'relative', width: width, height: height}}>
@@ -67,10 +67,7 @@ export default function App() {
         </div>
       </div>
       <div className="calculate_slider">
-          <div style={{
-            // width: "100%",
-            // marginLeft: "10%"
-            }}>
+          <div>
             <Nouislider
               range={{ min: sliderStart, max: sliderEnd }}
               start={firstValue}
@@ -90,107 +87,4 @@ export default function App() {
     </div>
   );
 }
-
-// const getStyles = stylesFactory((bcolor: any) => {
-//   return {
-//     calculate: css`
-//       text-align: center;
-//       background-image: ${bcolor};
-//       padding-bottom: 20px;
-//       width: 100%;
-//       height: 70%;
-//       max-height: 170px;
-//       border-radius: 20px 20px 0 0;
-//       border-radius: 20px 20px 0px 0px;
-//     `,
-//     calculate_wrap: css`
-//       height: 100%;
-//       display: flex;
-//       flex-direction: column;
-//       align-items: center;
-//     `,
-//     calculate_center: css`
-//       width: 100%;
-//       height: 100%;
-//       display: flex;
-//       flex-direction: column;
-//       align-items: center;
-//       justify-content: center;
-//       // border-radius: 20px 20px 0 0;
-//       // border-radius: 20px 20px 0px 0px;
-//   `,
-//     calculate_amount: css`
-//       color: rgba(255,255,255,0.6);
-//       font-size: 18px;
-//       font-weight: 400;
-//       // font-family: Roboto, sans-serif;
-//       line-height: 26px;
-//     `,
-//     calculate_text: css`
-//       color: #fff;
-//       font-weight: 700;
-//       font-family: Roboto, sans-serif;
-//       font-size: 45px;
-//       margin: 0;
-//     `,
-//     calculate_input: css`
-//       background: transparent;
-//       border: none;
-//       color: #fff;
-//       font-weight: 700;
-//       font-family: Roboto, sans-serif;
-//       font-size: 45px;
-//       // max-width: 170px;
-//       outline: none;
-//       // width: 154px;
-//       text-align: center;
-//     `,
-//     calculate_slider: css`
-//       padding: 0 60px;
-//     `,
-//     calculate_inner: css`
-//       display: flex;
-//       justify-content: space-between;
-//       padding: 0 90px;
-//     `,
-//     calculate_block: css`
-//       font-size: 14px;
-//       font-weight: 400;
-//       color: #a1afc3;
-//       text-align: center;
-//       line-height: 26px;
-//     `,
-//     calculate_block_span: css`
-//       font-weight: 700;
-//       font-size: 25px;
-//       // color: #1a273a;
-//     `,
-//     calculate_btn: css`
-//     & {
-//       font-weight: 700;
-//       text-decoration: none;
-//       text-transform: uppercase;
-//       color: rgba(255, 255, 255, 0.6);
-//       background: ${bcolor};
-//       width: auto;
-//       display: inline-block;
-//       padding: 10px 20px;
-//       border-radius: 10px;
-//       border: none;
-//     }
-//     &:hover {
-//       opacity: 0.7
-//     }
-//     `,
-//     calculate_btn_wrapper: css`
-//       width: 100%;
-//       height: 20%;
-//       display: flex;
-//       flex-direction: column;
-//       align-items: center;
-//       justify-content: center;
-//       margin: 20px 0;
-//     `,
-//   };
-// });
 
